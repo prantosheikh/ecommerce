f6d46cd9a466d505e9dbedb38bfe47ea3f21aec6
Pranto Sheikh <prantosheikh834@gmail.com>
2024-01-01 01:53:06 +0600

    create auth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+'use client'
+
+export default function Home() {
+   return (
+      <div>
+         <div className="p-4">
+            <Button>Click Me</Button>
+         </div>
+      </div>
+   )
+}
