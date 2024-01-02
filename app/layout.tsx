ee33ee8fa47618c4d72d3f90ab482d76521a5058
Pranto sheikh <prantosheikh834@gmail.com>
2024-01-02 18:29:26 +0600

    careate anather component office work completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,17 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-provider";
 import "./globals.css";
+import prismadb from "@/lib/prismadb";
 
 const inter = Inter({ subsets: ["latin"] });
+
+// const store = prismadb.store.file
 
 export const metadata: Metadata = {
 	title: "Admin Dashboard",
 	description: "Admin Dashboard",
 };
+
 
 export default function RootLayout({
 	children,
