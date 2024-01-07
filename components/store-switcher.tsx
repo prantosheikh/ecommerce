5595f5972943b6130eb1d852d61e33b5f2d74d17
Pranto sheikh <prantosheikh834@gmail.com>
2024-01-07 12:57:09 +0600

    d

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -1,24 +1,40 @@
 "use client";
 
-import { useStoreModal } from "@/hooks/use-store-modal";
-import { Popover, PopoverTrigger } from "./ui/popover";
-import { useParams, useRouter } from "next/navigation";
 import * as React from "react";
-import { Button } from "./ui/button";
-import { Store } from "lucide-react";
+import { Check, ChevronsUpDown, PlusCircle, Store } from "lucide-react";
+
 import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+	Command,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	CommandList,
+	CommandSeparator,
+} from "@/components/ui/command";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useParams, useRouter } from "next/navigation";
 
-type PopoverTriggerProps = React.ComponentPropsWithRef<typeof PopoverTrigger>;
+type PopoverTriggerProps = React.ComponentPropsWithoutRef<
+	typeof PopoverTrigger
+>;
 
-interface StoreSwitherProps extends PopoverTriggerProps {
+interface StoreSwitcherProps extends PopoverTriggerProps {
 	items: Record<string, any>[];
 }
 
-export default function StoreSwither({
+export default function StoreSwitcher({
 	className,
 	items = [],
-}: StoreSwitherProps) {
-	const storeModal = useStoreModal; // Fix the typo here
+}: StoreSwitcherProps) {
+	const storeModal = useStoreModal();
 	const params = useParams();
 	const router = useRouter();
 
@@ -49,9 +65,53 @@ export default function StoreSwither({
 					aria-label="Select a store"
 					className={cn("w-[200px] justify-between", className)}
 				>
-					<Store />
+					<Store className="mr-2 h-4 w-4" />
+					{currentStore?.label}
+					<ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
 				</Button>
 			</PopoverTrigger>
+			<PopoverContent className="w-[200px] p-0">
+				<Command>
+					<CommandList>
+						<CommandInput placeholder="Search store..." />
+						<CommandEmpty>No store found.</CommandEmpty>
+						<CommandGroup heading="Stores">
+							{formattedItems.map((store) => (
+								<CommandItem
+									key={store.value}
+									onSelect={() => onStoreSelect(store)}
+									className="text-sm"
+								>
+									<Store className="mr-2 h-4 w-4" />
+									{store.label}
+									<Check
+										className={cn(
+											"ml-auto h-4 w-4",
+											currentStore?.value === store.value
+												? "opacity-100"
+												: "opacity-0"
+										)}
+									/>
+								</CommandItem>
+							))}
+						</CommandGroup>
+					</CommandList>
+					<CommandSeparator />
+					<CommandList>
+						<CommandGroup>
+							<CommandItem
+								onSelect={() => {
+									setOpen(false);
+									storeModal.onOpen();
+								}}
+							>
+								<PlusCircle className="mr-2 h-5 w-5" />
+								Create Store
+							</CommandItem>
+						</CommandGroup>
+					</CommandList>
+				</Command>
+			</PopoverContent>
 		</Popover>
 	);
 }
