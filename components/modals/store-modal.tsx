1b72ee100f1054e7ae8d76d7a57c72afe3d30d66
Pranto Sheikh <prantosheikh834@gmail.com>
2024-01-09 21:15:14 +0600

    d

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -8,12 +8,12 @@ import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
+   Form,
+   FormControl,
+   FormField,
+   FormItem,
+   FormLabel,
+   FormMessage,
 } from "../ui/form";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
@@ -21,79 +21,79 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 
 const formSchema = z.object({
-	name: z.string().min(1),
+   name: z.string().min(1),
 });
 
 export const StoreModal = () => {
-	const storeModal = useStoreModal();
+   const storeModal = useStoreModal();
 
-	const [loading, setLoading] = useState(false);
+   const [loading, setLoading] = useState(false);
 
-	const form = useForm<z.infer<typeof formSchema>>({
-		resolver: zodResolver(formSchema),
-		defaultValues: {
-			name: "",
-		},
-	});
+   const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+         name: "",
+      },
+   });
 
-	const onSubmit = async (values: z.infer<typeof formSchema>) => {
-		setLoading(true);
-		try {
-			const response = await axios.post("/api/stores", values);
-			// console.log(response.data);
-			toast.success("Create Success ");
-		} catch (error) {
-			console.log(error);
-			toast.error("Somting Wrong");
-		} finally {
-			setLoading(false);
-		}
-	};
+   const onSubmit = async (values: z.infer<typeof formSchema>) => {
+      setLoading(true);
+      try {
+         const response = await axios.post("/api/stores", values);
+         // console.log(response.data);
+         window.location.assign(`${response.data.id}`)
+      } catch (error) {
+         console.log(error);
+         toast.error("Somting Wrong");
+      } finally {
+         setLoading(false);
+      }
+   };
 
-	return (
-		<Modal
-			title="Create Stores"
-			description="Add a new store to manage products and categories"
-			isOpen={storeModal.isOpen}
-			onClose={storeModal.onClose}
-		>
-			<div>
-				<div className="space-y-4 py-2 pb-4">
-					<Form {...form}>
-						<form onSubmit={form.handleSubmit(onSubmit)}>
-							<FormField
-								control={form.control}
-								name="name"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Name</FormLabel>
-										<FormControl>
-											<Input
-												disabled={loading}
-												placeholder="E-Commerce"
-												{...field}
-											/>
-										</FormControl>
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-							<div className="pt-6 space-x-2 flex items-center justify-end w-full">
-								<Button
-									disabled={loading}
-									variant="outline"
-									onClick={storeModal.onClose}
-								>
-									Cancel
-								</Button>
-								<Button type="submit" disabled={loading}>
-									Continue
-								</Button>
-							</div>
-						</form>
-					</Form>
-				</div>
-			</div>
-		</Modal>
-	);
+   return (
+      <Modal
+         title="Create Stores"
+         description="Add a new store to manage products and categories"
+         isOpen={storeModal.isOpen}
+         onClose={storeModal.onClose}
+      >
+         <div>
+            <div className="space-y-4 py-2 pb-4">
+               <Form {...form}>
+                  <form onSubmit={form.handleSubmit(onSubmit)}>
+                     <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                           <FormItem>
+                              <FormLabel>Name</FormLabel>
+                              <FormControl>
+                                 <Input
+                                    disabled={loading}
+                                    placeholder="E-Commerce"
+                                    {...field}
+                                 />
+                              </FormControl>
+                              <FormMessage />
+                           </FormItem>
+                        )}
+                     />
+                     <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                        <Button
+                           disabled={loading}
+                           variant="outline"
+                           onClick={storeModal.onClose}
+                        >
+                           Cancel
+                        </Button>
+                        <Button type="submit" disabled={loading}>
+                           Continue
+                        </Button>
+                     </div>
+                  </form>
+               </Form>
+            </div>
+         </div>
+      </Modal>
+   );
 };
