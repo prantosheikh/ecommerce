4cde8de694a3d33c33634f168b50b7644b0ef70e
Pranto Sheikh <prantosheikh834@gmail.com>
2024-01-10 22:28:15 +0600

    create

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -32,6 +32,7 @@ const SettingsPage: React.FC<SettingsPageProps> = async ({ params }) => {
       <div className="flex-col">
          <div className="flex-1 space-y-4 p-8 pt-6">
             <SettingsForm initialData={store} />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus voluptatum quo placeat sed id ex consequatur. Iste facere unde maxime.</p>
          </div>
       </div>
    );
