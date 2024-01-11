6c9f86205f7715ba68d77114e1bd12a8bbf31f31
Pranto sheikh <prantosheikh834@gmail.com>
2024-01-11 13:19:50 +0600

    create a new image uploading function

---
+++ components/ui/image-upload.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface ImageUploadProps {
+	disabled: boolean;
+	onChange: (value: string) => void;
+	onRemove: (value: string) => void;
+	value: string[];
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+	disabled,
+	onChange,
+	onRemove,
+	value,
+}) => {
+	const [isMounted, setIsMounted] = useState(false);
+
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
+
+	if (!isMounted) {
+		return null;
+	}
+	return <>Image Uploading</>;
+};
