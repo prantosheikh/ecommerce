bd4151d91f338c70c83726487d1da419e13e6e48
Pranto sheikh <prantosheikh834@gmail.com>
2024-01-14 12:02:18 +0600

    a

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -10,6 +10,7 @@ import { Separator } from "@/components/ui/separator";
 
 import { columns, BillboardColumn } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
 	data: BillboardColumn[];
@@ -38,7 +39,7 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
 			<DataTable searchKey="label" columns={columns} data={data} />
 			<Heading title="API" description="API Calls for Billboards" />
 			<Separator />
-			{/* <ApiList entityName="billboards" entityIdName="billboardId" /> */}
+			<ApiList entityName="billboards" entityIdName="billboardId" />
 		</>
 	);
 };
